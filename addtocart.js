b969666a94e8f8835c07f37ad58ece766359b7a4
nandhakumarsnk <127084372+nandhakumarsnk@users.noreply.github.com>
2023-03-09 11:47:28 +0530

    Update addtocart.js

--- addtocart.js
+++ addtocart.js
@@ -38,7 +38,7 @@ document.getElementById('root').innerHTML=categories.map((item)=>
                 </div>
                 <div class='bottom'>
                     <p>${title}</p>
-                    <h2><i class="fa-solid fa-indian-rupee-sign"></i> {price}.00</h2>`+
+                    <h2>$ ${price}.00</h2>`+
                     "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
                 `</div>
             </div>`      
